09b0b9d3f1ea5673738e36e1e54273b65b326a88
VarshiniManokaran <varshini6678@gmail.com>
2024-08-07 10:48:58 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,19 @@
+public class if_else {
+    public static void main(String[] args) {
+        int number=14;
+        if(number >80){
+        System.out.println("The number is positive");
+    }
+
+    else{
+        System.out.println("The number is nagative");
+    }
+}
+}
+
+
+
+    
+
+    
+
